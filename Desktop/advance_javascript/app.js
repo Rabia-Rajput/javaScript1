4c6a67611ebed5139f116c6402f0bc2d3da2aa50
Rabia Rajput <rajput.rabia@outlook.com>
2024-02-09 18:27:17 +0100

    use dir instead log

--- Desktop/advance_javascript/app.js
+++ Desktop/advance_javascript/app.js
@@ -2,6 +2,6 @@
 // alert();
 // window.alert(); //line 3 and 4 are same
 //console.log(document);
-//console.dir(document);
+console.dir(document);
 
 document.body.children[1].children[0].href = "https://google.com"
